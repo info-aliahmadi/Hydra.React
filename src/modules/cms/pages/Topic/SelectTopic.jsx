3f25af8e066d36ab35431f221e87c1a30ceecdaf
info.aliahmadi <info.aliahmadi@gmail.com>
2023-07-21 21:50:54 +0330

    work on topic tree

--- src/modules/cms/pages/Topic/SelectTopic.jsx
+++ src/modules/cms/pages/Topic/SelectTopic.jsx
@@ -3,10 +3,11 @@ import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import TopicsService from 'modules/cms/services/TopicService';
 import { Chip, MenuItem, OutlinedInput, Select } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, useTheme } from '@mui/system';
 
 export default function SelectTopic({ defaultValues, id, error, disabled }) {
   const [t] = useTranslation();
+  const theme = useTheme();
   const [loading, setLoading] = useState(true);
   const [options, setOptions] = useState([]);
   const [values, setValues] = useState([]);
@@ -21,17 +22,29 @@ export default function SelectTopic({ defaultValues, id, error, disabled }) {
   useEffect(() => {
     loadTopics();
   }, []);
-
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250
+      }
+    }
+  };
+  function getStyles(value, values, theme) {
+    return {
+      fontWeight: values.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium
+    };
+  }
   const Items = ({ items, space }) => {
     return items.map((item) => {
       if (item.childs.length > 0) {
         return (
-          <>
-            <MenuItem key={item.id} value={item.id}>
-              <span key={item.id}> {space}</span> {item.title}
-            </MenuItem>
-            <Items items={item.childs} space={<span>{space}&nbsp;&nbsp;</span>} key={'key' + item.id} />
-          </>
+          // <React.Fragment key={item.id}>
+          <MenuItem key={item.id} value={item.id}>
+            <span key={item.id}> {space}</span> {item.title}
+          </MenuItem>
         );
       } else {
         return (
@@ -45,27 +58,41 @@ export default function SelectTopic({ defaultValues, id, error, disabled }) {
   };
   const handleChange = (event) => {
     debugger;
-
-    setValues();
+    setValues(event.target.value);
   };
+  const handleDelete = (event) => {
+    setValues(event.target.value);
+  };
+
   return (
     <Select
       id={id}
       key={id + loading}
       multiple
       value={values || ''}
-      label="Age"
+      label={''}
       onChange={handleChange}
+      MenuProps={MenuProps}
       input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
       renderValue={(selected) => (
         <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-          {selected.map((value) => (
-            <Chip key={value} label={value} />
-          ))}
+          {selected.map((value) => {
+            let title = options.find((x) => x.id == value).title;
+            return <Chip key={title} label={title} />;
+          })}
         </Box>
       )}
     >
-      <Items items={options} space={<span></span>} key={'item.id'}></Items>
+      {options.map((item) => {
+        return (
+          // <React.Fragment key={item.id}>
+          <MenuItem key={item.id} value={item.id} style={getStyles(item.id, values, theme)}>
+            {item.title}
+          </MenuItem>
+          // </React.Fragment>
+        );
+      })}
+      {/* <Items items={options} space={<span></span>} key={'options'}></Items> */}
     </Select>
   );
 }
