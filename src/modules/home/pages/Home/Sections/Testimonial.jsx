e856d9cba86ee01d420aeed0d5cb709be612928e
info.aliahmadi <info.aliahmadi@gmail.com>
2023-10-26 17:10:40 +0330

    ss

--- src/modules/home/pages/Home/Sections/Testimonial.jsx
+++ src/modules/home/pages/Home/Sections/Testimonial.jsx
@@ -1,13 +1,17 @@
-import { Button, Grid, MobileStepper, Paper, Typography } from '@mui/material';
+import { Avatar, Button, Grid, MobileStepper, Paper, Typography } from '@mui/material';
 import { Box, Container } from '@mui/system';
-import Feature1Image from 'assets/images/Feature-1.png';
-import Feature2Image from 'assets/images/Feature-2.png';
-import Feature3Image from 'assets/images/Feature-3.png';
+import WebflowLogo from 'assets/images/webflow-logo.svg';
+
 import WaveTestimonialImage from 'assets/images/wave-testimonial.svg';
 import WestRoundedIcon from '@mui/icons-material/WestRounded';
 import EastRoundedIcon from '@mui/icons-material/EastRounded';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
 import 'react';
 import { useState } from 'react';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 export default function Testimonial() {
   const steps = [
@@ -42,51 +46,76 @@ export default function Testimonial() {
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
   return (
     <Box style={{ background: '#A1D4F8' }}>
       <Container>
         <Grid xs={12} sm={12} md={12} lg={12} xd={12} style={{ position: 'relative' }}>
-          <img src={WaveTestimonialImage} alt="" height={'100%'} width={'100%'} />
-          <Paper
-            square
-            elevation={0}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              height: 50,
-              pl: 2,
-              bgcolor: 'background.default',
-              position: 'absolute',
-              top: '40%'
-            }}
-          >
-            <Grid item rowSpacing={{ xs: 7, sm: 7, md: 5, lg: 7, xd: 7 }} xs={10} sm={10} md={10} lg={10} xd={10}>
-              <Typography variant="h3">{steps[activeStep].description}</Typography>
-            </Grid>
-            <Grid item rowSpacing={{ xs: 7, sm: 7, md: 5, lg: 7, xd: 7 }} xs={10} sm={10} md={10} lg={10} xd={10}>
-              <Typography variant="body1">
-                {steps[activeStep].name} / {steps[activeStep].company}
-              </Typography>
-            </Grid>
-            {/* <Box sx={{ height: 255, maxWidth: 400, width: '100%', p: 2 }}>
-              {steps[activeStep].name} / {steps[activeStep].company}
-            </Box> */}
-          </Paper>
+          <img src={WaveTestimonialImage} alt="" height={'100%'} width={'100%'} style={{ position: 'absolute' }} />
+          <AutoPlaySwipeableViews index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
+            {steps.map((step, index) => (
+              <Grid
+                key={'x-' + index}
+                item
+                container
+                justifyContent="center"
+                alignItems="center"
+                sx={
+                  {
+                    //position: 'absolute',
+                    //top: '30%'
+                  }
+                }
+              >
+                <Grid item xs={9} sm={8} md={7} lg={7} xd={7}>
+                  <Typography variant="h3" textAlign={'center'}>
+                    {step.description}
+                  </Typography>
+                </Grid>
+                <Grid item xs={11} sm={10} md={8} lg={7} xd={7}>
+                  <Grid container mt={{ xs: 0, sm: 4, md: 4, lg: 5, xd: 5 }} alignItems="center" justifyContent="center">
+                    <Grid item xs={3} sm={2} md={2} lg={1} xd={1} p={{ xs: 2, sm: 2, md: 3, lg: 4, xd: 4 }}>
+                      <Avatar sx={{ width: 56, height: 56 }}></Avatar>
+                    </Grid>
+                    <Grid item xs={4} sm={4} md={4} lg={5} xd={5} p={{ xs: 2, sm: 2, md: 3, lg: 4, xd: 4 }}>
+                      <Typography variant="body1" fontWeight={600}>
+                        {step.name}
+                      </Typography>
+                      <Typography variant="body1">{step.company}</Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={3}
+                      sm={3}
+                      md={3}
+                      lg={3}
+                      xd={3}
+                      style={{ borderLeft: '1px solid #2c302e' }}
+                      p={{ xs: 2, sm: 2, md: 3, lg: 4, xd: 4 }}
+                    >
+                      <img src={WebflowLogo} alt="Company Logo" />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            ))}
+          </AutoPlaySwipeableViews>
         </Grid>
         <Grid xs={12} sm={12} md={12} lg={12} xd={12}>
           <MobileStepper
-            variant="text"
             steps={maxSteps}
             position="static"
             activeStep={activeStep}
             nextButton={
               <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                <EastRoundedIcon style={{ position: 'relative', top: '40%' }} />
+                <EastRoundedIcon />
               </Button>
             }
             backButton={
               <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-                <WestRoundedIcon style={{ position: 'relative', top: '40%' }} />
+                <WestRoundedIcon />
               </Button>
             }
           />
--- src/themes/HomePageTheme/overrides/Typography.js
+++ src/themes/HomePageTheme/overrides/Typography.js
@@ -38,6 +38,40 @@ export default function Typography(theme) {
             fontSize: '3.3rem'
           }
         },
+        h2: {
+          [theme.breakpoints.up('xs')]: {
+            fontSize: '1.9rem'
+          },
+          [theme.breakpoints.up('sm')]: {
+            fontSize: '2.0rem'
+          },
+          [theme.breakpoints.up('md')]: {
+            fontSize: '2.3rem'
+          },
+          [theme.breakpoints.up('lg')]: {
+            fontSize: '2.7rem'
+          },
+          [theme.breakpoints.up('xl')]: {
+            fontSize: '2.875rem'
+          }
+        },
+        h3: {
+          [theme.breakpoints.up('xs')]: {
+            fontSize: '1.2rem'
+          },
+          [theme.breakpoints.up('sm')]: {
+            fontSize: '1.3rem'
+          },
+          [theme.breakpoints.up('md')]: {
+            fontSize: '1.5rem'
+          },
+          [theme.breakpoints.up('lg')]: {
+            fontSize: '1.9rem'
+          },
+          [theme.breakpoints.up('xl')]: {
+            fontSize: '2rem'
+          }
+        },
         gutterBottom: {
           marginBottom: 12
         }
